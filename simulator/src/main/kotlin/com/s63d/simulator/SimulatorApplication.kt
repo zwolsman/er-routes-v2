1c17c78fdef8e12195cd982364e35b62c0257bbe
Zwolsman,Marvin M.W <m.zwolsman@student.fontys.nl>
2018-03-03 15:23:34 +0100

    Rewrote main class

--- simulator/src/main/kotlin/com/s63d/simulator/SimulatorApplication.kt
+++ simulator/src/main/kotlin/com/s63d/simulator/SimulatorApplication.kt
@@ -4,8 +4,11 @@ import org.springframework.boot.autoconfigure.SpringBootApplication
 import org.springframework.boot.runApplication
 
 @SpringBootApplication
-class SimulatorApplication
-
-fun main(args: Array<String>) {
-    runApplication<SimulatorApplication>(*args)
+class SimulatorApplication {
+    companion object {
+        @JvmStatic
+        fun main(args: Array<String>) {
+            runApplication<SimulatorApplication>(*args)
+        }
+    }
 }
