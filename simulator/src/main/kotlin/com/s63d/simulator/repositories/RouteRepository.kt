2c6df76f4cd5c172cb02d500ad162a507dcbc980
Zwolsman,Marvin M.W <m.zwolsman@student.fontys.nl>
2018-03-03 15:21:45 +0100

    Added route repo

---
+++ simulator/src/main/kotlin/com/s63d/simulator/repositories/RouteRepository.kt
@@ -0,0 +1,7 @@
+package com.s63d.simulator.repositories
+
+import com.s63d.domain.database.RoutesItem
+import org.bson.types.ObjectId
+import org.springframework.data.mongodb.repository.MongoRepository
+
+interface RouteRepository : MongoRepository<RoutesItem, ObjectId>
