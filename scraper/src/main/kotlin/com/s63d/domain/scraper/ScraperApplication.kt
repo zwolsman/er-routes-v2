d2cd2cc1f7d5660c7b5447fd64d191d5c6e6559d
Zwolsman,Marvin M.W <m.zwolsman@student.fontys.nl>
2018-03-03 14:19:00 +0100

    Refactored main class Added database name

--- scraper/src/main/kotlin/com/s63d/domain/scraper/ScraperApplication.kt
+++ scraper/src/main/kotlin/com/s63d/domain/scraper/ScraperApplication.kt
@@ -4,8 +4,11 @@ import org.springframework.boot.autoconfigure.SpringBootApplication
 import org.springframework.boot.runApplication
 
 @SpringBootApplication
-class ScraperApplication
-
-fun main(args: Array<String>) {
-    runApplication<ScraperApplication>(*args)
+class ScraperApplication {
+    companion object {
+        @JvmStatic
+        fun main(args: Array<String>) {
+            runApplication<ScraperApplication>(*args)
+        }
+    }
 }
